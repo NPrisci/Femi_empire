fa475f8851cf5c8c66b1f5ad19f014ee85b84d3b
Priscillia <priscilialauress@gmail.com>
2026-09-07 10:47:36 +0200

    nouveau

--- config/database.php
+++ config/database.php
@@ -8,14 +8,24 @@ function getDB(): PDO
 
     if ($pdo === null) {
 
-        // Railway fournit MYSQLHOST, MYSQLPORT, etc.
-        // En local, on utilise les valeurs classiques XAMPP/WAMP.
+        
         $dbHost = getenv('MYSQLHOST') ?: 'localhost';
         $dbPort = getenv('MYSQLPORT') ?: '3306';
         $dbName = getenv('MYSQLDATABASE') ?: 'femiempire';
         $dbUser = getenv('MYSQLUSER') ?: 'root';
         $dbPass = getenv('MYSQLPASSWORD') ?: '';
 
+        if (!$dbHost || !$dbName || !$dbUser || !$dbPass) {
+            error_log('Variables MySQL Railway manquantes');
+            error_log('MYSQLHOST=' . ($dbHost ?: 'NOT_SET'));
+            error_log('MYSQLPORT=' . ($dbPort ?: 'NOT_SET'));
+            error_log('MYSQLDATABASE=' . ($dbName ?: 'NOT_SET'));
+            error_log('MYSQLUSER=' . ($dbUser ?: 'NOT_SET'));
+            error_log('MYSQLPASSWORD=' . ($dbPass ? 'SET' : 'NOT_SET'));
+
+            throw new RuntimeException('Configuration MySQL manquante');
+        }
+        
         $dsn = "mysql:host={$dbHost};port={$dbPort};dbname={$dbName};charset=" . DB_CHARSET;
 
         $options = [
