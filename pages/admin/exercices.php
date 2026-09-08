1b3957dbfa88b840910c3f3e6d3778b26bbb25ce
Priscillia <priscilialauress@gmail.com>
2026-09-08 11:36:45 +0200

    exercices

--- pages/admin/exercices.php
+++ pages/admin/exercices.php
@@ -2,7 +2,7 @@
 // pages/admin/exercices.php
 // Gestion des exercices
 
-// require_once __DIR__ . '/includes/header.php';
+require_once __DIR__ . '/includes/header.php';
 
 $pdo = getDB();
 
@@ -811,7 +811,7 @@ $formations =
 $flash_messages = getFlash();
 
 
-require_once __DIR__ . '/includes/header.php';
+
 ?>
 
 
